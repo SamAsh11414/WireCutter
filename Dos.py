5e75434601af700f534345f57a980b089c82e7ba
Samuel Ashkenas <72979934+SamAsh11414@users.noreply.github.com>
2020-11-17 11:20:54 -0500

    Quality of life fixes

--- Dos.py
+++ Dos.py
@@ -1,12 +1,14 @@
 import threading
 import socket
 
+print('  __      __.__               _________         __    __                \n /  \    /  \__|______   ____ \_   ___ \ __ ___/  |__/  |_  ___________ \n \   \/\/   /  \_  __ \_/ __ \/    \  \/|  |  \   __\   __\/ __ \_  __ \ \n  \        /|  ||  | \/\  ___/\     \___|  |  /|  |  |  | \  ___/|  | \/ \n   \__/\  / |__||__|    \___  >\______  /____/ |__|  |__|  \___  >__|   \n        \/                  \/        \/                       \/      ')
+
     # This is where you will input the target IP and port
-target = ''
-port = 0
+target = input('What is your target IP ')
+port = int(input('What port are you targeting '))
 
     #if you would like, you can also try and use a fake_ip, this will not help hide your identity however.
-fake_ip = ''
+fake_ip = int(input('What is your fake IP '))
 
 already_connected = 0
 
--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # WireCutter
  A simple Dos panel made in python.
 
+ Use is very simple, just follow the comments in the file labled "Dos.py".
+ Run this code in a IDE and input the needed information. Use is very simple.
+
+ This is to be used ONLY LOCALLY
+
 ```
 
  __      __.__               _________         __    __                
